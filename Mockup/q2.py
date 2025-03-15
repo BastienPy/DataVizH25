1b3614b46c0c0f99593b34ec198cfbb679ff19ff
alexisbord <alexis.bord094@gmail.com>
2025-03-15 14:24:15 -0400

    q2 a jour

--- Mockup/q2.py
+++ Mockup/q2.py
@@ -4,116 +4,196 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import numpy as np
+import plotly.colors as pc
 
 def get_dataframe(path):
     data = pd.read_csv(path)
-    data["track_album_release_date"] = pd.to_datetime(data["track_album_release_date"], format="mixed", errors="coerce")
-    data = data[data["track_album_release_date"].dt.year >= 1970] # On ne garde que les musiques après 1970, car il n'y a pas assez d'échantillons avant
+    data["track_album_release_date"] = pd.to_datetime(data["track_album_release_date"])
+    data = data.groupby("playlist_genre").apply(lambda x: x.nlargest(500, "track_popularity")).reset_index(drop=True)
+    excluded_artists = [
+        "The Sleep Specialist", "Nature Sounds", "Natural Sound Makers", "Mother Nature Sound FX",
+        "Rain Recordings", "Pinetree Way", "Aquagirl", "Rain Sounds FX", "Relax Meditate Sleep",
+        "Life Sounds Nature"
+    ]
+    data = data[~data["track_artist"].isin(excluded_artists)]
+    data = data[data["track_album_release_date"].dt.year >= 1970]
     return data
 
-
-def get_figure(genre=None):
+def get_figure():
     data = get_dataframe("./dataset/spotify_songs_clean.csv")
-    if genre:
-        data = data[data['playlist_genre'] == genre]
-    # Sélectionner uniquement les colonnes numériques
     numeric_data = data.select_dtypes(include=['number']).drop(columns=['key', 'track_popularity', 'mode'])
-    # Calculer la matrice de corrélation
     corr_matrix = numeric_data.corr()
-    corr_matrix = corr_matrix.applymap(lambda x: round(x, 2))  # Optional: round to 2 decimal places for readability
-    corr_matrix = corr_matrix.clip(lower=-1, upper=1)  # Ensure the scale is between -1 and 1
-    # Afficher la matrice avec plotly
-    fig = px.imshow(corr_matrix,
-                    text_auto=False,
+    corr_matrix[:] = 0  
+    mask = np.triu(np.ones(corr_matrix.shape), k=0)
+    masked_corr_matrix = corr_matrix.where(mask == 0, np.nan)
+    masked_corr_matrix = masked_corr_matrix.iloc[1:, :-1]  
+    norm_values = np.zeros(masked_corr_matrix.shape)
+    custom_colorscale = [
+        [0.0, "lightgray"],
+        [0.1, "darkblue"],
+        [0.2, "purple"],
+        [0.4, "red"],
+        [0.6, "cyan"],
+        [0.8, "orange"],
+        [1.0, "green"]
+    ]
+    color_mappings = {
+        1.0: [("loudness", "energy"), ("acousticness", "energy"), ("acousticness", "loudness"), 
+              ("valence", "danceability"), ("valence", "energy")],
+        0.8: [("instrumentalness", "energy"), ("instrumentalness", "loudness"), ("instrumentalness", "acousticness")],
+        0.6: [("speechiness", "energy"), ("speechiness", "loudness"), ("tempo", "speechiness")],
+        0.4: [("valence", "loudness")],
+        0.2: [("energy", "danceability"), ("loudness", "danceability"), ("valence", "acousticness")],
+        0.1: [("duration_ms", "instrumentalness"), ("duration_ms", "loudness"), 
+              ("liveness", "energy"), ("liveness", "loudness"), ("tempo", "energy")]
+    }
+    for value, label_pairs in color_mappings.items():
+        for row_label, col_label in label_pairs:
+            if row_label in masked_corr_matrix.index and col_label in masked_corr_matrix.columns:
+                row_idx = masked_corr_matrix.index.get_loc(row_label)
+                col_idx = masked_corr_matrix.columns.get_loc(col_label)
+                norm_values[row_idx, col_idx] = value  
+    norm_values = np.where(np.tril(np.ones(norm_values.shape)), norm_values, np.nan)
+    fig = px.imshow(norm_values,
                     aspect="auto",
-                    title=f"Correlation Matrix for {genre}" if genre else "Correlation Matrix",
-                    color_continuous_scale="Viridis",
-                    zmin=-0.65,
-                    zmax=0.8)
-    fig.update_layout(width=800, height=600)
+                    title=f"Corrélations importantes",
+                    labels=dict(x="", y=""),
+                    x=masked_corr_matrix.columns,
+                    y=masked_corr_matrix.index,
+                    color_continuous_scale=custom_colorscale
+                    )
+    fig.update_traces(z=norm_values, colorscale=custom_colorscale)
+    fig.update_coloraxes(showscale=False)
+    legend_items = [
+        ("All Genres", "green"),
+        ("EDM", "darkblue"),
+        ("Latin", "red"),   
+        ("R&B", "purple"),
+        ("Rap", "orange"),
+        ("Rock", "cyan"),
+        ("No Correlation", "lightgray")
+    ]
+    annotations = []
+    shapes = []
+    x_offset = 1.05
+    y_offset = 1.2
+    spacing = -0.08
+    box_size = 0.05
+    for i, (label, color) in enumerate(legend_items):
+        shapes.append(
+            dict(
+                type="rect",
+                x0=x_offset - box_size + 0.07, y0=y_offset + i * spacing - box_size / 2 - 0.04,
+                x1=x_offset + 0.07, y1=y_offset + i * spacing + box_size / 2 - 0.04,
+                xref="paper", yref="paper",
+                fillcolor=color,
+                line=dict(width=0)
+            )
+        )
+        annotations.append(
+            dict(
+                x=x_offset, y=y_offset + i * spacing,
+                xref="paper", yref="paper",
+                text=f"<b>{label}</b>",
+                showarrow=False,
+                font=dict(size=14, color="black"),
+                align="left"
+            )
+        )
+    
+    # Ajouter une note indiquant que la pop n’a pas de corrélations détectées
+    annotations.append(
+        dict(
+            x=0, y=y_offset + (len(legend_items) * spacing) - 0.89,
+            xref="paper", yref="paper",
+            text="<i>Aucune relation particulière pour la pop</i>",
+            showarrow=False,
+            font=dict(size=12, color="gray"),
+            align="left"
+        )
+    )
+
+    fig.update_layout(annotations=annotations, shapes=shapes)
+    fig.update_layout(
+        width=600,
+        height=500,
+        plot_bgcolor="white",  
+        paper_bgcolor="white",
+        xaxis=dict(showgrid=False, zeroline=False),  
+        yaxis=dict(showgrid=False, zeroline=False)  
+    )
     return fig
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H4("Corrélation entre les caractéristiques audio", style={"fontWeight": "bold", "fontSize": "30px"}),
-    dcc.Graph(id="graph", figure = get_figure()),
+    html.Div([
+        dcc.Graph(id="graph-all", figure=get_figure()),
+        dcc.Graph(id="scatter-plot")
+    ], style={"display": "flex", "flex-wrap": "wrap", "gap": "20px"})
 ])
 
 @app.callback(
     Output("scatter-plot", "figure"),
-    [Input("graph", "clickData")]
+    [Input("graph-all", "clickData")]
 )
-def update_scatter_plot(clickData):
-    if clickData is None:
+def update_plot(all_clickData):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
         return dash.no_update
     
-    data = get_dataframe("./dataset/spotify_songs_clean.csv")
-    numeric_data = data.select_dtypes(include=['number'])
-    corr_matrix = numeric_data.corr()
-
-    x_feature = clickData['points'][0]['x']
-    y_feature = clickData['points'][0]['y']
+    click_data = ctx.triggered[0]['value']
+    
+    if click_data is None:
+        return dash.no_update
+    
+    x_feature = click_data['points'][0]['x']
+    y_feature = click_data['points'][0]['y']
 
     if x_feature == y_feature:
         return dash.no_update
 
-    scatter_fig = px.scatter(data, x=x_feature, y=y_feature, title=f"Scatter plot of {x_feature} vs {y_feature}")
-    scatter_fig.update_layout(width=800, height=600)
-    parallel_fig = px.parallel_coordinates(data, dimensions=[x_feature, y_feature], title=f"Parallel Coordinates of {x_feature} and {y_feature}")
-    parallel_fig.update_layout(width=800, height=600)
+    data = get_dataframe("./dataset/spotify_songs_clean.csv")
+    selected = click_data['points'][0]['z']
+
+    if selected == 1.0:
+        selected_genre = 'all'
+    elif selected == 0.8:
+        selected_genre = 'rap'
+    elif selected == 0.6:
+        selected_genre = 'rock'
+    elif selected == 0.4:
+        selected_genre = 'latin'
+    elif selected == 0.2:
+        selected_genre = 'r&b'
+    elif selected == 0.1:
+        selected_genre = 'edm'
+
+    if selected_genre == 'all':
+        scatter_fig = px.scatter(data, x=x_feature, y=y_feature, hover_data=['track_name', 'track_artist'], color='playlist_genre', title=f"Scatter plot of {x_feature} vs {y_feature}")
+        scatter_fig = px.parallel_coordinates(data, dimensions=['loudness', 'acousticness', 'energy', 'valence', 'danceability'], color='track_popularity', title=f"Parallel coordinates plot for {selected_genre}")
+    if selected_genre == 'rap':
+        genre_data = data[data['playlist_genre'] == selected_genre]
+        scatter_fig = px.parallel_coordinates(genre_data, dimensions=['energy', 'instrumentalness','loudness', 'acousticness'], color='track_popularity', title=f"Parallel coordinates plot for {selected_genre}")
+    if selected_genre == 'rock':
+        genre_data = data[data['playlist_genre'] == selected_genre]
+        scatter_fig = px.parallel_coordinates(genre_data, dimensions=['loudness', 'speechiness','energy', 'tempo'], color='track_popularity', title=f"Parallel coordinates plot for {selected_genre}")
+    if selected_genre == 'latin':
+        genre_data = data[data['playlist_genre'] == selected_genre]
+        scatter_fig = px.parallel_coordinates(genre_data, dimensions=['valence', 'loudness'], color='track_popularity', title=f"Parallel coordinates plot for {selected_genre}")
+    if selected_genre == 'r&b':
+        genre_data = data[data['playlist_genre'] == selected_genre]
+        scatter_fig = px.parallel_coordinates(genre_data, dimensions=['energy', 'danceability', 'loudness', 'valence', 'acousticness'], color='track_popularity', title=f"Parallel coordinates plot for {selected_genre}")
+    if selected_genre == 'edm':
+        genre_data = data[data['playlist_genre'] == selected_genre]
+        scatter_fig = px.parallel_coordinates(genre_data, dimensions=['tempo', 'energy', 'duration_ms', 'liveness', 'loudness', 'instrumentalness'], color='track_popularity', title=f"Parallel coordinates plot for {selected_genre}")
+    
+    scatter_fig.update_layout(width=600, height=600)
+
     return scatter_fig
 
-app.layout = html.Div([
-    html.H4("Corrélation entre les caractéristiques audio", style={"fontWeight": "bold", "fontSize": "30px"}),
-    html.Div([
-        html.Button("All", id="all-button", n_clicks=0),
-        html.Button("Pop", id="pop-button", n_clicks=0),
-        html.Button("Rap", id="rap-button", n_clicks=0),
-        html.Button("Rock", id="rock-button", n_clicks=0),
-        html.Button("Latin", id="latin-button", n_clicks=0),
-        html.Button("R&B", id="rnb-button", n_clicks=0),
-        html.Button("EDM", id="edm-button", n_clicks=0),
-    ], style={"display": "flex", "gap": "5px", "margin-bottom": "20px"}),
-    html.Div([
-        dcc.Graph(id="graph", figure=get_figure()),
-        dcc.Graph(id="scatter-plot")
-    ], style={"display": "flex"})
-])
-
-@app.callback(
-    Output("graph", "figure"),
-    [Input("all-button", "n_clicks"),
-     Input("pop-button", "n_clicks"),
-     Input("rap-button", "n_clicks"),
-     Input("rock-button", "n_clicks"),
-     Input("edm-button", "n_clicks"),
-     Input("rnb-button", "n_clicks"),
-     Input("latin-button", "n_clicks")]
-)
-def update_graph(all_clicks, pop_clicks, rock_clicks, hiphop_clicks, electronic_clicks, rnb_clicks, jazz_clicks):
-    ctx = dash.callback_context
-
-    if not ctx.triggered:
-        return get_figure()
-    else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    genre = None
-    if button_id == "pop-button":
-        genre = "pop"
-    elif button_id == "rap-button":
-        genre = "rap"
-    elif button_id == "rock-button":
-        genre = "rock"
-    elif button_id == "edm-button":
-        genre = "edm"
-    elif button_id == "rnb-button":
-        genre = "r&b"
-    elif button_id == "edm-button":
-        genre = "edm"
-
-    return get_figure(genre)
-
-
-app.run_server(debug=False)
+if __name__ == '__main__':
+    app.run_server(debug=True)
